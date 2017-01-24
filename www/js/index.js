4fb533dbacf4e28759f4e393eb0764d686711727
Jamison, Robert <Bob.Jamison@pearson.com>
2017-01-24 16:07:06 -0600

    Appearance changes

--- www/js/index.js
+++ www/js/index.js
@@ -19,6 +19,7 @@
 
 function Graph() {
 
+	var that = this;
 	this.canvas = document.getElementById('graph');
 	this.ctx = this.canvas.getContext("2d");
 
@@ -36,7 +37,7 @@ function Graph() {
 		var left = 35;
 		var right = w - 35;
 		ctx.font = 'bold 12pt Courier';
-		
+
 		/**
 		 * Draw columns
 		 */
@@ -45,8 +46,8 @@ function Graph() {
 		ctx.strokeStyle = "yellow";
 		var x = left;
 		var i;
-		for (i = 0; i <= nrCol ; i++) {
-			ctx.beginPath()
+		for (i = 0; i <= nrCol; i++) {
+			ctx.beginPath();
 			ctx.moveTo(x, top);
 			ctx.lineTo(x, bottom);
 			ctx.stroke();
@@ -61,7 +62,7 @@ function Graph() {
 		nrCol = 10;
 		var freqDiff = (end - start) / nrCol;
 		diff = (right - left) / nrCol;
-		for (i = 0; i <= nrCol ; i++) {
+		for (i = 0; i <= nrCol; i++) {
 			ctx.fillStyle = "white";
 			ctx.fillText(freq.toFixed(0), x, h - 5);
 			x += diff;
@@ -74,8 +75,8 @@ function Graph() {
 		var nrRow = 20;
 		diff = (bottom - top) / nrRow;
 		var y = top;
-		for (i = 0; i <= nrRow ; i++) {
-			ctx.beginPath()
+		for (i = 0; i <= nrRow; i++) {
+			ctx.beginPath();
 			ctx.moveTo(left, y);
 			ctx.lineTo(right, y);
 			ctx.stroke();
@@ -85,12 +86,12 @@ function Graph() {
 		/**
 		 * Draw SWR and R numbers
 		 */
-		var y = bottom + 2;
+		y = bottom + 2;
 		var swr = 1.0;
 		var swrDiff = 5.0 / nrRow;
 		var r = 0;
 		var rDiff = 300 / nrRow;
-		for (i = 0; i <= nrRow ; i++) {
+		for (i = 0; i <= nrRow; i++) {
 			ctx.fillStyle = "red";
 			ctx.fillText(swr.toFixed(1), 2, y);
 			ctx.fillStyle = "green";
@@ -103,18 +104,33 @@ function Graph() {
 	};
 
 	Object.defineProperties(this, {
-		"start": { 
-			get: function () { return this._start },
-			set: function(v) { this._start = v; this.redraw(); }
+		"start": {
+			get: function() {
+				return this._start;
 			},
-		"end": { 
-			get: function () { return this._end; },
-			set: function(v) { this._end = v; this.redraw(); }
+			set: function(v) {
+				this._start = v;
+				this.redraw();
+			}
+		},
+		"end": {
+			get: function() {
+				return this._end;
 			},
-		"step": { 
-			get: function () { return this._step; },
-			set: function(v) { this._step = v; this.redraw(); }
+			set: function(v) {
+				this._end = v;
+				this.redraw();
+			}
+		},
+		"step": {
+			get: function() {
+				return this._step;
 			},
+			set: function(v) {
+				this._step = v;
+				this.redraw();
+			}
+		},
 	});
 
 	this.start = 13900;
@@ -136,188 +152,245 @@ function Graph() {
 		this.redraw();
 	};
 
+	window.onresize = function(){
+		that.redraw();
+	};
+
+	setTimeout(function() {
+		that.redraw();
+	}, 2000);
+
 }
- 
+
 
 function Open60App() {
 
-		var that = this;
+	var that = this;
 
-		Object.defineProperties(this, {
-			"ready": { 
-				get: function () { return this._ready; },
-				set: function(v) { this._ready = v }
-			 },
-			"connected": { 
-				get: function () { return false; }  // bluetoothSerial.isConnected;0
-			 },
-			"start": { 
-				get: function () { return this._start; },
-				set: function(v) { this._start = v; if (this.graph) this.graph.start = v; }
-			 },
-			"end": { 
-				get: function () { return this._end; },
-				set: function(v) { this._end = v;  if (this.graph) this.graph.end = v; }
+	Object.defineProperties(this, {
+		"ready": {
+			get: function() {
+				return this._ready;
 			},
-			"step": { 
-				get: function () { return this._step; },
-				set: function(v) { this._step = v;  if (this.graph) this.graph.step = v; }
-			 },
-		});
-
-		this.ready = false;
-		this.start = 14000.0;
-		this.end = 14350.0;
-		this.step = 5;
-
-		function trace(msg) {
-			console.log(msg);
-		}
-    
-		function error(msg) {
-			console.log("error: " + msg);
-		}
-
-		this.addGraph = function() {
-			this.graph = new Graph(this.start, this.end, this.step);
-		}
-    
-		this.connect = function(device) {
-			function receive(data) {
-				that.receive(data);
+			set: function(v) {
+				this._ready = v;
 			}
-			function subscribeFailure(msg) {
-				error("subscribe: " + msg);
-			}
-			function connectSuccess() {
-				bluetoothSerial.subscribe('\n', receive, subscribeFailure)
-			}
-			function connectFailure(msg) {
-				error("connect: " + msg);
-			}
-			bluetoothSerial.connect(device.id, connectSuccess. connectFailure);
-		};
-
-		this.disconnect = function() {
-			function success() {
-
-			}
-			function failure(msg) {
-
-			}
-			if (bluetoothSerial.isConnected) {
-				bluetoothSerial.unsubscribe();
-				bluetoothSerial.disconnect(success, failure);
-			}
-		};
-
-		this.receive = function(data) {
-			console.log(data);
-		};
-
-		this.send = function(msg) {
-			function success() {
-
-			}
-			function failure(msg) {
-				error("send: " + msg);
-			}
-			bluetoothSerial.write(msg, success, failure);
-		};
-
-		this.findMini60AndConnect = function() {
-			function success(devices) {
-				var dev = devices.find(function(d) {
-					var name = d.name;
-					return name.startsWith("MINI60");
-				});
-				if (!dev) {
-					error("Paired device 'MINI60' not found");
-				}
-				that.connect(dev);
-			}
-			function failure(msg) {
-				error("cannot list paired devices");
-			}
-			bluetoothSerial.list(success, failure);
-		};
-
-		this.scan = function() {
-			var start = this.start * 1000;
-			var end = this.end * 1000;
-			var step = this.step * 1000;
-			var cmd = "scan " + start + " " + end + " " + step;
-			this.send(cmd);
-		};
-
-		var toolbarComponent = {
-			template: '\
-			<div>\
-			<span>{{ connected }} </span>\
-			<button v-on:click="connect()">connect</button>\
-			<button v-on:click="disconnect()">disconnect</button>\
-			<span>start</span><input v-model="start">\
-			<span>end</span><input v-model="end">\
-			<span>step</span><input v-model="step">\
-			<button v-on:click="scan()">scan</button>\
-			</div>\
-			',
-			data: function() {
-				return {
-					connected: that.connected,
-					start: that.start,
-					end: that.end,
-					step: that.step,
-				}
+		},
+		"connected": {
+			get: function() {
+				return this._connected;
 			},
-			methods: {
-				connect: function() {
-					that.findMini60AndConnect();
-				},
-				disconnect: function() {
+			set: function(v) {
+				this._connected = v;
+			}
+		},
+		"start": {
+			get: function() {
+				return this._start;
+			},
+			set: function(v) {
+				this._start = v;
+				if (this.graph) this.graph.start = v;
+			}
+		},
+		"end": {
+			get: function() {
+				return this._end;
+			},
+			set: function(v) {
+				this._end = v;
+				if (this.graph) this.graph.end = v;
+			}
+		},
+		"step": {
+			get: function() {
+				return this._step;
+			},
+			set: function(v) {
+				this._step = v;
+				if (this.graph) this.graph.step = v;
+			}
+		},
+	});
+
+	this.ready = false;
+	this.connected = false;
+	this.start = 14000.0;
+	this.end = 14350.0;
+	this.step = 5;
+
+	function trace(msg) {
+		console.log(msg);
+	}
+
+	function error(msg) {
+		console.log("error: " + msg);
+		if (navigator.showToast) {
+			navigator.showToast(msg);
+		}
+	}
+
+	this.addGraph = function() {
+		this.graph = new Graph(this.start, this.end, this.step);
+	};
+
+	this.checkConnect = function() {
+		function success() {
+			that.connected = true;
+		}
+		function failure() {
+			that.connected = false;
+		}
+		bluetoothSerial.isConnected(success, failure);
+	};
+
+	this.connect = function(device) {
+		function receive(data) {
+			that.receive(data);
+		}
+
+		function subscribeFailure(msg) {
+			error("subscribe: " + msg);
+		}
+
+		function connectSuccess() {
+			that.connected = true;
+			bluetoothSerial.subscribe('\n', receive, subscribeFailure);
+		}
+
+		function connectFailure(msg) {
+			that.connected = false;
+			error("connect: " + msg);
+		}
+		bluetoothSerial.connect(device.address, connectSuccess, connectFailure);
+	};
+
+	this.disconnect = function() {
+		function success() {
+			that.connected = false;
+		}
+
+		function failure(msg) {
+			that.connected = false;
+		}
+		if (that.connected) {
+			bluetoothSerial.unsubscribe();
+			bluetoothSerial.disconnect(success, failure);
+		}
+	};
+
+	this.receive = function(data) {
+		console.log(data);
+	};
+
+	this.send = function(msg) {
+		if (!this.connected) {
+			return;
+		}
+		function success() {
+
+		}
+
+		function failure(msg) {
+			error("send: " + msg);
+		}
+		bluetoothSerial.write(msg, success, failure);
+	};
+
+	this.findMini60AndConnect = function() {
+		function success(devices) {
+			var dev = devices.find(function(d) {
+				var name = d.name;
+				return name.startsWith("MINI60");
+			});
+			if (!dev) {
+				error("Paired device 'MINI60' not found");
+			}
+			that.connect(dev);
+		}
+
+		function failure(msg) {
+			error("cannot list paired devices");
+		}
+		bluetoothSerial.list(success, failure);
+	};
+
+	this.scan = function() {
+		var start = this.start * 1000;
+		var end = this.end * 1000;
+		var step = this.step * 1000;
+		var cmd = "scan " + start + " " + end + " " + step;
+		this.send(cmd);
+	};
+
+	var toolbarComponent = {
+		template: '<div class="toolbar">' +
+			'<button class="cmd-button" v-on:click="toggleConnect()">{{ connected ? "disconnect" : "connect" }}</button>' +
+			'<span class="number-input">start<input v-model="start"></span>' +
+			'<span class="number-input">end<input v-model="end"></span>' +
+			'<span class="number-input">step<input v-model="step"></span>' +
+			'<button class="cmd-button" v-on:click="scan()">scan</button>' +
+			'</div>',
+		data: function() {
+			return {
+				connected: that.connected,
+				start: that.start,
+				end: that.end,
+				step: that.step,
+			};
+		},
+		methods: {
+			toggleConnect: function() {
+				if (that.connected) {
 					that.disconnect();
-				},
-				scan: function() {
-					that.scan();	
+				} else {
+					that.findMini60AndConnect();
 				}
+			},
+			disconnect: function() {
+				that.disconnect();
+			},
+			scan: function() {
+				that.scan();
 			}
-		};
+		}
+	};
 
-		Vue.component('toolbar-component', toolbarComponent);
-		
-		this.refreshDevices = function() {
-			if (typeof bluetoothSerial !== "undefined") {
-				function success(devices) {
-					btData.devices = devices.slice(0);
-				}
-				function failure(msg) {
-					console.log("bluetooth list error: " + msg);
-				}
-				bluetoothSerial.list(success, failure);
-			} else {
-				btData.devices = [
-					{
-					name: "device1"
-					},
-					{
-					name: "device2"
-					},
-					{
-					name: "device3"
-					}
-				];
-			}
-		};
-		
-		function onDeviceReady() {
-			that.ready = true;
+	Vue.component('toolbar-component', toolbarComponent);
+
+	this.refreshDevices = function() {
+		function success(devices) {
+			btData.devices = devices.slice(0);
+		}
+		function failure(msg) {
+			console.log("bluetooth list error: " + msg);
 		}
 
-		if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-			document.addEventListener("deviceready", onDeviceReady, false);
+		if (typeof bluetoothSerial !== "undefined") {
+			bluetoothSerial.list(success, failure);
 		} else {
-			onDeviceReady(); //this is a browser
+			btData.devices = [{
+					name: "device1"
+				},
+				{
+					name: "device2"
+				},
+				{
+					name: "device3"
+				}
+			];
 		}
+	};
+
+	function onDeviceReady() {
+		that.ready = true;
+	}
+
+	if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+		document.addEventListener("deviceready", onDeviceReady, false);
+	} else {
+		onDeviceReady(); //this is a browser
+	}
 
 }
 
@@ -327,8 +400,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	open60app.addGraph();
 	var app = new Vue({
 		el: '#app',
-		data: {
-		}
+		data: {}
 	});
 });
-
